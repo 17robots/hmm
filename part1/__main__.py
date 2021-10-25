a22fe1c888c59c5600bb6a7bd870d09292304bf3
17robots <mdray@ameritech.net>
2021-10-24 22:30:40 -0400

    Update part1 command

--- part1/__main__.py
+++ part1/__main__.py
@@ -2,11 +2,15 @@ import pandas as pd
 import click
 
 @click.command()
-@click.argument()
-@click.argument()
-@click.argument()
-def func():
-    ...
+@click.argument("filename")
+# @click.argument()
+# @click.argument()
+def func(filename):
+    # read in the steps
+    try
+        pd.readCSV(filename)
+    except:
+        print('Failed to open file')
 
 if __name__ == '__main__':
-    print('Hello World')
+    func()
